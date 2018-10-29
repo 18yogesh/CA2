c9f94be75bf99f996ec211a6f68c0ef938cff929
18yogesh <44490122+18yogesh@users.noreply.github.com>
2018-10-29 16:50:27 +0530

    Add files via upload

---
+++ Program 1/router/main.js
@@ -0,0 +1,67 @@
+
+var Contact = require("./../models/Schema")[0];
+
+
+
+module.exports = function(app,passport){
+    
+    app.get("/",(req,res)=>{
+        res.render("index.html");
+    });
+    app.post('/login',passport.authenticate('local'),function(req, res) {
+                    // If this function gets called, authentication was successful.
+                    // `req.user` contains the authenticated user.
+                    res.redirect('/chat');
+    });
+
+    app.get("/about",(req,res)=>{
+        res.render("about.html");
+    });
+    app.get("/contact",(req,res)=>{
+        Contact.find({},{},function(err,docs){
+            res.render("contact.html",{contacts:docs});
+         });
+           // res.render("contact.html");
+      
+    });
+    app.get("/chat",(req,res)=>{
+        res.render("chat.html");
+    });
+   app.post("/",(req,res)=>{
+    res.send(req.body.comment);
+   });
+   app.post("/contact",(req,res)=>{
+     const Joi = require("joi");
+
+     var data = {name:req.body.name,email:req.body.email,message:req.body.message};
+     const schema = Joi.object().keys({
+        name:Joi.string().required().label("Please enter your name"),
+        email:Joi.string().email().required(),
+        message:Joi.string().required()
+        
+
+     });
+     Joi.validate(data,schema,(err,value)=>{
+        if(err){
+            res.status(422).json({
+                status:'error',
+                message:'Invalid Request Data'+err.message,
+                data:value
+            });
+        }else{
+            Contact.create(data,function(err, record){
+                if(err) throw err;
+                Contact.find({},{},function(err,docs){
+                   res.render("contact.html",{contacts:docs});
+                });
+       
+            });
+            
+        }
+     });
+     
+    
+    
+   });
+    
+}
